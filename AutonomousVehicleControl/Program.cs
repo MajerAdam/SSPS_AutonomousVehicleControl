2ac695367b19c65ecd76d03d382d78425e0836ce
theonlydejf <krcmar.dav@gmail.com>
2022-03-14 10:08:23 +0100

    implementovano vse krom chyb

--- AutonomousVehicleControl/Program.cs
+++ AutonomousVehicleControl/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AutonomousVehicleControl
 {
@@ -6,12 +8,19 @@ namespace AutonomousVehicleControl
     {
         static void Main(string[] args)
         {
-            
+
         }
     }
 
     public class Silnice
     {
+        public Silnice(double maxRychlost, string jmeno, double delka)
+        {
+            MaxRychlost = maxRychlost;
+            Jmeno = jmeno;
+            Delka = delka;
+        }
+
         private string jmeno;
         public string Jmeno
         {
@@ -20,7 +29,7 @@ namespace AutonomousVehicleControl
         }
 
         private double maxRychlost;  // maximální možná rychlost na sinici
-        public double MaxRychlost 
+        public double MaxRychlost
         {
             get => maxRychlost;
             set => maxRychlost = value;
@@ -51,111 +60,222 @@ namespace AutonomousVehicleControl
             // TODO zmenit rychlost
             auto.Rychlost = MaxRychlost;
         }
-           
+
     }
 
     public class Most : Silnice
     {
+        public Most(string jmeno, double delka) : base(50, jmeno, delka)
+        {
+        }
+
         public override void Entered(Auto auto) // zmení se rychlost podle počasí
         {
             base.Entered(auto);
-            throw new NotImplementedException();
+
+            auto.Rychlost = auto.RidiciSystem.GetMostRychlost(auto);
         }
 
     }
 
     public class Tunel : Silnice
     {
-        public override void Left(Auto auto) // změní se stav světel a rychlost 
+        public Tunel(string jmeno, double delka) : base(70, jmeno, delka)
+        {
+        }
+
+        public override void Left(Auto auto) // změní se stav světel a rychlost
         {
             base.Left(auto);
-            throw new NotImplementedException();
+            auto.StavSvetel = false;
         }
 
         public override void Entered(Auto auto) // změní se stav světel a rychlost
         {
             base.Entered(auto);
-            throw new NotImplementedException();
+            auto.StavSvetel = true;
         }
-        
 
-    }
-
-    public class BeznaCesta : Silnice
-    {
 
     }
 
     public class Lokace
     {
-        //TODO kostra lokace
+        public double UjetaVzdalenost { get; set; }
+        public Silnice AktualniSilnice { get; set; }
     }
 
-    public class RoadChangedEventArgs : EventArgs
+    public class SilniceChangedEventArgs : EventArgs
     {
         public Silnice Silnice { get; set; }
         public Lokace VehicleLocation { get; set; }
     }
 
+    public class PoruchaEventArgs : EventArgs
+    {
+        public ChybovyKod ChybovyKod { get; set; }
+    }
+
+    public enum ChybovyKod
+    {
+        HodneSpatny,
+        MaloSpatny
+    }
+
     public class Auto
     {
-        public event EventHandler<RoadChangedEventArgs> NewRoadEntered;
+        public event EventHandler<SilniceChangedEventArgs> NewSilniceEntered;
+        public event EventHandler<SilniceChangedEventArgs> DruhSilniceChanged;
+        public event EventHandler<EventArgs> RychlostChanged; //TODO
+        public event EventHandler<EventArgs> StavSvetelChanged; //TODO
+        public event EventHandler<PoruchaEventArgs> OnPorucha;
+        public event EventHandler<EventArgs> OnTrasaDokoncena;
 
-        private double rychlost;
+        public string SPZ { get; }
+        public List<Silnice> Trasa = new List<Silnice>();
+        public Silnice CurrSilnice { get; private set; }
+
+        internal Auto(RidiciSystem ridiciSystem, List<Silnice> trasa, string spz)
+        {
+            RidiciSystem = ridiciSystem;
+            Trasa = trasa;
+            SPZ = spz;
+        }
+
         public double Rychlost
         {
-            get => rychlost;
-            set => rychlost = value;
+            get;
+            set;
         }
 
-        private double trasa;
-        public double Trasa
-        {
-            get => trasa;
-            set => trasa = value;
-        }
-
-        private Lokace poloha;
         public Lokace Poloha
         {
-            get => poloha;
-            set => poloha = value;
+            get;
+            set;
         }
 
-        private bool stavSvetel;
         public bool StavSvetel
         {
-            get => stavSvetel;
-            set => stavSvetel = value;
+            get;
+            set;
         }
 
-        public void porucha() // vyvolá metodu z Ridiciho systemu podle typu poruchy
+        public RidiciSystem RidiciSystem
         {
-            throw new NotImplementedException();
+            get;
+            private set;
+        }
+
+        private Random rnd = new Random();
+        public ChybovyKod VyvolatPoruchu() // vyvolá metodu z Ridiciho systemu podle typu poruchy
+        {
+            ChybovyKod chyba = (ChybovyKod)rnd.Next(2);
+            RidiciSystem.VyresPoruchu(this, chyba);
+            OnPorucha?.Invoke(this, new PoruchaEventArgs() { ChybovyKod = chyba });
+            return chyba;
+        }
+
+        public async Task ProvedTrasu()
+        {
+            Silnice lastSilnice = null;
+            for (int i = 0; i < Trasa.Count; i++)
+            {
+                CurrSilnice = Trasa[i];
+
+                NewSilniceEntered?.Invoke(this, new SilniceChangedEventArgs() { Silnice = CurrSilnice, VehicleLocation = Poloha });
+                if (!lastSilnice.GetType().Equals(CurrSilnice.GetType()))
+                    DruhSilniceChanged?.Invoke(this, new SilniceChangedEventArgs() { Silnice = CurrSilnice, VehicleLocation = Poloha });
+                lastSilnice = CurrSilnice;
+
+                CurrSilnice.Entered(this);
+
+                bool staneSeChyba = rnd.Next(0, 100) == 0;
+
+                double cilovaVzdalenost = CurrSilnice.Delka;
+
+                if (staneSeChyba)
+                    cilovaVzdalenost *= rnd.NextDouble();
+
+                double dobaJizdy = cilovaVzdalenost * Rychlost * 3600 * 1000 * RidiciSystem.TimeScale;
+
+                await Task.Delay((int)dobaJizdy);
+
+                if (staneSeChyba)
+                    if (VyvolatPoruchu() == ChybovyKod.HodneSpatny)
+                        break;
+
+                CurrSilnice.Left(this);
+            }
+
+            OnTrasaDokoncena?.Invoke(this, EventArgs.Empty);
         }
     }
 
     public class RidiciSystem
     {
-        public double GetMostRychlost() // Vytvoří rychlost podle pomocí počasí (zeptá se MeteroStredisko)
+        public static double TimeScale = 1;
+
+        public double GetMostRychlost(Auto auto) // Vytvoří rychlost podle pomocí počasí (zeptá se MeteroStredisko)
+        {
+            switch (MeteoStredisko.GetPocasi(auto.Poloha))
+            {
+                case Pocasi.Dobry:
+                    return 70;
+                case Pocasi.Stredni:
+                    return 50;
+                case Pocasi.Spatny:
+                    return 30;
+            }
+            return 50;
+        }
+
+        public void VyresPoruchu(Auto auto, ChybovyKod chybovyKod)
+        {
+            switch (chybovyKod)
+            {
+                case ChybovyKod.HodneSpatny:
+                    VyresTezkouPoruchu(auto);
+                    break;
+                case ChybovyKod.MaloSpatny:
+                    VyresLehkouPoruchu(auto);
+                    break;
+            }
+        }
+
+        private void VyresTezkouPoruchu(Auto auto) //
         {
             throw new NotImplementedException();
         }
-        public void HorsiPorucha() //
+
+        private Random rnd = new Random();
+        private void VyresLehkouPoruchu(Auto auto)
         {
-            throw new NotImplementedException();
+            //TODO pridej do trasy cestu k servisu a tam dej novy auto
+            int index = auto.Trasa.IndexOf(auto.CurrSilnice);
+            auto.Trasa.RemoveRange(index + 1, auto.Trasa.Count - index);
+            auto.Trasa.Add(new Silnice(30, "Cesta do servisu", rnd.NextDouble() * 5 + 1));
+
         }
-        public void LepsiPorucha()
+
+        public Auto CreateAuto(List<Silnice> trasa, string spz)
         {
-            throw new NotImplementedException();
+            return new Auto(this, trasa, spz);
         }
     }
 
-    public class MeteroStredisko
+    public enum Pocasi
     {
-        public string GetPocasi() // dostaneme pocasi
+        Dobry,
+        Stredni,
+        Spatny
+    }
+
+    public static class MeteoStredisko
+    {
+        private static Random rnd = new Random();
+        public static Pocasi GetPocasi(Lokace lokace) // dostaneme pocasi
         {
-            throw new NotImplementedException();
+            return (Pocasi)rnd.Next(3);
         }
     }
 
@@ -169,7 +289,7 @@ namespace AutonomousVehicleControl
         {
             throw new NotImplementedException();
         }
-        public double GetTrasuKServisu(double poloha) // Dostane cestu od aouta k nejbližšímu servisu
+        public double GetTrasaKServisu(double poloha) // Dostane cestu od aouta k nejbližšímu servisu
         {
             throw new NotImplementedException();
         }
