36e9d3904214eb7623ac87ac42e1c51787afb479
theonlydejf <krcmar.dav@gmail.com>
2022-03-21 08:16:43 +0100

    organizace, pridan projekt visualiser

---
+++ Visualiser/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AutonomousVehicleControl;
+
+namespace Visualiser
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Auto> auta = new List<Auto>();
+        public List<List<Silnice>> trasy = new List<List<Silnice>>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
